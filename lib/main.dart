428b64b992afd4b2db43b6f623a4bf1caa3892cb
Celestin Niyonsaba <celestinniyonsaba@Celestins-iMac.local>
2022-07-05 16:24:37 +0200

    orientaion using Mediaquery

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Responsive app',
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
--- lib/my-home-page.dart
+++ lib/my-home-page.dart
@@ -8,33 +8,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  // bool isDesktop(BuildContext context) =>
-  //     MediaQuery.of(context).size.width >= 600;
-
-  // bool isMobile(BuildContext context) =>
-  //     MediaQuery.of(context).size.width < 600;
   @override
   Widget build(BuildContext context) {
-    bool isDesktop = MediaQuery.of(context).size.width >= 600;
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Responsive App'),
-        ),
-        body: Center(
-          child: Row(children: [
-            // if(isDesktop(context)) //
-            if (isDesktop)
-              Container(
-                color: Colors.blueAccent,
-                width: 200,
-                child: const Center(child: Text('SIDEBAR')),
-              ),
-            Expanded(
-                child: Container(
-                    // color: Colors.grey,
-                    child: const Center(child: Text('MAIN BODY'))))
-          ]),
-        ));
+      body: OrientationBuilder(builder: (context, orientation) {
+        var isPortrait = orientation == Orientation.portrait;
+        return isPortrait
+            ? Container(
+                color: Colors.blue,
+                child: const Center(
+                  child: Text('Portrait'),
+                ),
+              )
+            : Container(
+                color: Colors.greenAccent,
+                child: const Center(
+                  child: Text('Landscape'),
+                ),
+              );
+      }),
+    );
   }
 }
